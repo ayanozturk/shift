3ee5aef634f7ee5588d6a036a528e23e96aac75a
Ayan Ozturk <ayanozturk@gmail.com>
2020-08-20 23:54:13 +0100

    'wip'

--- assets/js/CreateShift.js
+++ assets/js/CreateShift.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { DatePicker, Space, Input, Button } from "antd";
+
+const { RangePicker } = DatePicker;
+const format = 'HH:mm';
 
 class CreateShift extends React.Component {
     render() {
-        return <h2>Shift Stuff! <span>❤️</span></h2>;
+        return (
+            <>
+
+                <Space direction="vertical" size={12}>
+                    <Input placeholder="Shift Name" />
+                    <RangePicker
+                        picker="time"
+                        minuteStep={15}
+                        format={format}
+                    />
+                    <Button type="primary">Create Shift</Button>
+                </Space>
+            </>
+        );
     }
 }
 
