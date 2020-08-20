91d6c2bc4530717e4746d032e5e82f448bbdfbf5
Ayan Ozturk <ayanozturk@gmail.com>
2020-08-21 00:24:25 +0100

    date range with react

--- assets/js/CreateShift.js
+++ assets/js/CreateShift.js
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react';
 import ReactDom from 'react-dom'
-import { DatePicker, Space, Input, Button } from "antd";
+import {Button, DatePicker, Input, Space} from "antd";
+import moment from 'moment';
 
 const { RangePicker } = DatePicker;
 const format = 'HH:mm';
 
-class CreateShift extends React.Component {
-    render() {
-        return (
-            <>
-
-                <Space direction="vertical" size={12}>
-                    <Input placeholder="Shift Name" />
-                    <RangePicker
-                        picker="time"
-                        minuteStep={15}
-                        format={format}
-                    />
-                    <Button type="primary">Create Shift</Button>
-                </Space>
-            </>
-        );
-    }
+function CreateShift() {
+    const defaultStart = moment('09:00', 'HH:mm')
+    const defaultEnd = defaultStart.clone().add(8, 'hours')
+    const [shiftRange, setShiftRange] = useState([defaultStart, defaultEnd])
+    return (
+        <>
+            <Space direction="vertical" size={12}>
+                <Input placeholder="Shift Name"/>
+                <RangePicker
+                    value={shiftRange}
+                    picker="time"
+                    minuteStep={15}
+                    format={format}
+                    onCalendarChange={value => {
+                        setShiftRange(value)
+                    }}
+                />
+                <Button type="primary">Create Shift</Button>
+            </Space>
+        </>
+    );
 }
 
 ReactDom.render(<CreateShift />, document.getElementById('create-shift'))
